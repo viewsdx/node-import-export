a48c872376be247e6f4f77053354609e05bb959d
Darío Javier Cravero <dario@uxtemple.com>
2017-07-03 12:32:32 +0100

    fix: relative paths

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
+const path = require('path')
+
+let index = process.argv[2]
+if (!path.isAbsolute(index)) {
+  index = path.join(process.cwd(), index)
+}
+
 require('import-export')
-require(process.argv[2])
+require(index)
